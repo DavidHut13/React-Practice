d8b392649498924ab502ba02266f94430c726d01
David <dhutto_11@live.com>
2020-06-10 21:52:56 -0500

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Burger from '../../components/Burger/Burger.js'
+
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <>
+           <Burger>
+               
+           </Burger>
+            <div>
+                Build Controls
+            </div>
+            
+            </>
+        );
+    }
+
+}
+
+export default BurgerBuilder;
